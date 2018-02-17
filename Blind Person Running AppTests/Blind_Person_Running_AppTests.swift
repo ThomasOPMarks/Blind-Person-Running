2cd91550ef60c91a5df5aac1b41e622d8ec2c907
student <student@students-macbook-pro.local>
2018-02-16 18:56:55 -0700

    Initial Commit

---
+++ Blind Person Running AppTests/Blind_Person_Running_AppTests.swift
@@ -0,0 +1,36 @@
+//
+//  Blind_Person_Running_AppTests.swift
+//  Blind Person Running AppTests
+//
+//  Created by student on 2/16/18.
+//  Copyright © 2018 Blind Institute of Technology. All rights reserved.
+//
+
+import XCTest
+@testable import Blind_Person_Running_App
+
+class Blind_Person_Running_AppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
