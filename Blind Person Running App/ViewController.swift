47ae30ca99878d30dd411a4093fbda3d90b3b87e
student <student@students-macbook-pro.local>
2018-02-16 22:12:11 -0700

    Version 0.1, GUI Demonstration

--- Blind Person Running App/ViewController.swift
+++ Blind Person Running App/ViewController.swift
@@ -7,9 +7,13 @@
 //
 
 import UIKit
-
+//Accessibility must be mainted on this app.
 class ViewController: UIViewController {
 
+    @IBOutlet weak var TitleLabel: UILabel!
+    @IBOutlet weak var StatusLabel: UILabel!
+    @IBOutlet weak var ConnectButton: UIButton!
+    @IBOutlet weak var DisconnectButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,21 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func ConnectPushed(_ sender: Any) {
+        ConnectButton.isEnabled = false;
+        ConnectButton.accessibilityHint = "A session is already taking place";
+        StatusLabel.text = "Status: Connected";
+        DisconnectButton.isEnabled = true;
+        DisconnectButton.accessibilityHint = "Double tap to end the current running session and disconnect from the grid";
+    }
+    
+    @IBAction func EndSessionPushed(_ sender: Any) {
+        ConnectButton.isEnabled = true;
+        ConnectButton.accessibilityHint = "Double tap to connect to the grid and start a running session";
+        StatusLabel.text = "Status: Not Connected";
+        DisconnectButton.isEnabled = false;
+        DisconnectButton.accessibilityHint = "There is no current running session to end";
+        
+    }
 }
 
