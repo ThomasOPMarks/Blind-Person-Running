0b213c2493da37c98abd6e98c4320a0653e1740e
student <student@students-macbook-pro.local>
2018-05-16 21:38:11 -0600

    Added reverse, removed lane buttons

--- Blind Person Running App/ViewController.swift
+++ Blind Person Running App/ViewController.swift
@@ -17,15 +17,12 @@ class ViewController: UIViewController {
     @IBOutlet weak var ConnectButton: UIButton!
     @IBOutlet weak var DisconnectButton: UIButton!
     @IBOutlet weak var LaneNumberLabel: UILabel!
-    @IBOutlet weak var Lane1: UIButton!
-    @IBOutlet weak var Lane2: UIButton!
-    @IBOutlet weak var Lane3: UIButton!
-    @IBOutlet weak var Lane4: UIButton!
-    @IBOutlet weak var Lane5: UIButton!
-    @IBOutlet weak var Lane6: UIButton!
+    @IBOutlet weak var ReverseButton: UIButton!
+    
     var CurrentLane: Int = 1
     var reop = false
     var help = 10
+    var direction = true
     
     var connection = NetworkBuffer()
     var MessageBack: String = " "
@@ -95,7 +92,19 @@ class ViewController: UIViewController {
         print(MessageBack)
     }
     
-    @IBAction func Lane1Pushed(_ sender: Any) {
+    @IBAction func ReverseButtonPushed(_ sender: Any) {
+        let text = "Reverse"
+        let sending = "\(text)".data(using: .ascii)!
+        if (ReverseButton.accessibilityHint == "Click to Reverse Direction. Current direction is clockwise."){
+            ReverseButton.accessibilityHint = "Click to Reverse Direction. Current direction is counter clockwise."
+        }
+        else{
+            ReverseButton.accessibilityHint = "Click to Reverse Direction. Current direction is clockwise."
+        }
+        _ = sending.withUnsafeBytes { connection.outputStream.write($0, maxLength: sending.count) }
+        
+    }
+    /*@IBAction func Lane1Pushed(_ sender: Any) {
         if(reop){
             print("Changed it")
         }
@@ -200,7 +209,7 @@ class ViewController: UIViewController {
             lanes[i].isEnabled = true;
             lanes[i].accessibilityHint = "Push to run in lane \(i + 1), currently set to run in lane \(CurrentLane)"
         }
-    }
+    }*/
 }
 
 extension ViewController: NetworkBufferDelegate{
